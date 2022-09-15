aafa3efe26fcbdcc7957185d90453037333f7043
skrj <skrj@example.com>
2022-09-16 00:42:52 +0900

    [UPDATE] ファイルの修正

--- packinstall/dockerinstall.sh
+++ packinstall/dockerinstall.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-sudo apt install -y curl                \\
-                    gnupg-agent         \\
-                    ca-certificates     \\
-                    apt-transport-https \\
-                    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install -y docker-ce
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
 sudo usermod -aG docker ${USER}
 su - ${USER}
