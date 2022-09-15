54504e1468f165b96e9a1314aabd1bfb38ccdcdc
skrj <skrj@example.com>
2022-09-16 01:17:49 +0900

    [UPDATE] 一部修正

--- packinstall/anaconda3.sh
+++ packinstall/anaconda3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -OL https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh --output anacodna3.sh
-sudo bash anacodna3.sh -bfu -p /opt/anacodna3
+curl -o anaconda3.sh -OL https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+sudo bash anacodna3.sh -bfu -p /opt/anaconda3
+sudo ln -s /opt/anaconda3/bin/conda /usr/bin/conda
 rm -f anacodna3.sh
