2fa28355c28557c572469f6b46a166cff2f61771
skrj <skrj@example.com>
2022-09-23 16:21:03 +0900

    [UPDATE]

--- packinstall/anaconda3.sh
+++ packinstall/anaconda3.sh
@@ -4,5 +4,5 @@ pkgname=anaconda3
 curl -o anaconda3.sh -OL https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 export CONDAPATH=/opt/anaconda3/bin
 export PATH=$PATH:$CONDAPATH
-. anaconda3.sh -bfu -p /opt/anaconda3
+bash anaconda3.sh -bfu -p /opt/anaconda3
 rm -f anaconda3.sh
