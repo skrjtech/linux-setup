bf7608d5eed49d1b7fe494f514c4ac2edc1cf8d1
skrj <skrj@example.com>
2022-09-23 16:46:06 +0900

    [UPDATE]

--- browser/googlechromeinstall.sh
+++ browser/googlechromeinstall.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
