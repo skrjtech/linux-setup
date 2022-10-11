7c19c44c1d4a5a9c2ac65ca3afb70483abea2707
skrjtech <skrjtech@gmail.com>
2022-10-12 02:40:04 +0900

    dockerinstall.sh

--- packinstall/dockerinstall.sh
+++ packinstall/dockerinstall.sh
@@ -7,3 +7,9 @@ sudo apt update
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
 su - ${USER}
+
+# Docker Compose 
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
