a7cf75846d636003dad6a25e2f3333c69edb9aa5
skrjtech <skrjtech@skrtech.com>
2022-11-28 02:42:14 +0900

    修正

--- installer/docker.sh
+++ installer/docker.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-# sudo apt install -y gnome-terminal
-# sudo apt remove docker-desktop
-# rm -r $HOME/.docker/desktop
-# sudo rm /usr/local/bin/com.docker.cli
-# sudo apt purge docker-desktop
-# sudo apt-get update
-# sudo apt-get install -y ca-certificates curl gnupg lsb-release
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# sudo apt-get update
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-# sudo usermod -aG docker ${USER}
-# su - ${USER}
 
-# Version
-VERSION=$1
 sudo apt install -y gnome-terminal
 sudo apt remove docker-desktop
 rm -r $HOME/.docker/desktop
@@ -29,10 +12,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
-if [ $VERSION = "" ]; then
+if [ $# == 1 ]; then
     sudo apt-get install -y docker-ce docker-ce-cli
 else
-    sudo apt-get install -y docker-ce=$VERSION docker-ce-cli=$VERSION
+    sudo apt-get install -y docker-ce=$1 docker-ce-cli=$1
 fi
 sudo apt-get install -y containerd.io docker-compose-plugin
 sudo usermod -aG docker ${USER}
