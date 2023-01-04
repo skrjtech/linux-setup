1d5cbea9649c6b4527264108018f3042e120ac7a
skrjtech <skrjtech@skrtech.com>
2023-01-05 04:07:45 +0900

    update

--- archives/dockerProjects/yolact/docker/yolactWeightsDownload.sh
+++ archives/dockerProjects/yolact/docker/yolactWeightsDownload.sh
@@ -15,3 +15,6 @@ Download 1UYy3dMapbH1BnmtZU4WH1zbYgOzzHHf_ /yolact/weights/yolact_base_54_800000
 Download 1lE4Lz5p25teiXV-6HdTiOJSnS7u7GBzg /yolact/weights/yolact_im700_54_800000.pth
 Download 1ZPu1YR2UzGHQD0o1rEqy-j5bmEm3lbyP /yolact/weights/yolact_plus_resnet50_54_800000.pth
 Download 15id0Qq5eqRbkD-N3ZjDZXdCvRyIaHpFB /yolact/weights/yolact_plus_base_54_800000.pth
+Download 1tvqFPd4bJtakOlmn-uIA492g2qurRChj /yolact/weights/resnet101_reducedfc.pth
+Download 1Jy3yCdbatgXa5YYIdTCRrSV0S9V5g1rn /yolact/weights/resnet50-19c8e357.pth
+Download 17Y431j4sagFpSReuPNoFcj9h7azDTZFf /yolact/weights/darknet53.pth
