13688cb791cfb73ca97541bc0510f1565341f82e
skrjtech <skrjtech@skrtech.com>
2023-01-07 08:20:06 +0900

    update

--- archives/dockerProjects/nordvpn/entrypoint.sh
+++ archives/dockerProjects/nordvpn/entrypoint.sh
@@ -3,11 +3,16 @@
 # NORDTOKEN
 # LOCALIP
 
-/etc/init.d/nordvpn start && /etc/init.d/squid start
+/etc/init.d/nordvpn start
+sleep 5
+/etc/init.d/squid start
+sleep 5
 
 nordvpn login --token $NORDVPN_TOKEN
 nordvpn whitelist add subnet $LOCALIP
 nordvpn c
+
+sleep 5
 
 NORDVPN_IP=$(ip -4 a show nordlynx | grep -oP '(?<=inet\s)\d+(\.\d+){3}\/(.\d+)')
 IPADD=$(echo $NORDVPN_IP | sed -e 's/\//\\\//')
