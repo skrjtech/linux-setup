240c700c6096970228cdbd30e7f6fe7910b0f74e
skrjtech <skrjtech@skrtech.com>
2023-01-07 09:24:19 +0900

    update

--- archives/dockerProjects/nordvpn/entrypoint.sh
+++ archives/dockerProjects/nordvpn/entrypoint.sh
@@ -9,12 +9,19 @@ sleep 5
 sleep 5
 
 nordvpn login --token $NORDVPN_TOKEN
-nordvpn whitelist add subnet $LOCALIP
+nordvpn whitelist add subnet $(ip -4 a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}\/(.\d+)')
 nordvpn c
-
 sleep 5
 
-NORDVPN_IP=$(ip -4 a show nordlynx | grep -oP '(?<=inet\s)\d+(\.\d+){3}\/(.\d+)')
-IPADD=$(echo $NORDVPN_IP | sed -e 's/\//\\\//')
-sed -ie "s/XXX.XXX.XXX.XXX/${IPADD}/g" /etc/squid/squid.conf
+# LOCAL ETH0
+IP=$(ip -4 a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}\/(.\d+)' | sed -e 's/\//\\\//')
+sed -ie "s/ETH0IP/${IP}/g" /etc/squid/squid.conf
+# LOCAL WLAN0
+IP=$(ip -4 a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}\/(.\d+)' | sed -e 's/\//\\\//')
+sed -ie "s/WLAN0IP/${IP}/g" /etc/squid/squid.conf
+# NORDVPN
+IP=$(ip -4 a show nordlynx | grep -oP '(?<=inet\s)\d+(\.\d+){3}\/(.\d+)' | sed -e 's/\//\\\//')
+sed -ie "s/NORDVPNIP/${IP}/g" /etc/squid/squid.conf
+
+# Reload And Restart
 /etc/init.d/squid reload && /etc/init.d/squid restart
