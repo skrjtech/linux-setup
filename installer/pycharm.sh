7e2970d9fb5b0a8b864502c5e2f70a364073976d
skrjtech <skrjtech@skrtech.com>
2023-01-14 23:45:21 +0900

    update

--- installer/pycharm.sh
+++ installer/pycharm.sh
@@ -3,11 +3,12 @@ sudo apt update && sudo apt -y upgrade
 # java
 sudo apt install -y default-jre
 sudo apt install -y default-jdk
+
+PYCHARM_VERSION=2022.2.1
 # pycahrm
 sudo apt-get install -y curl
-curl -OL https://download.jetbrains.com/python/pycharm-community-2022.2.1.tar.gz
-sudo tar xzf pycharm-community-2022.2.1.tar.gz -C /opt/
-rm pycharm-community-2022.2.1.tar.gz
-sudo mv /opt/pycharm-community-2022.2.1 /opt/pycharm
+curl -OL https://download.jetbrains.com/python/pycharm-community-$PYCHARM_VERSION.tar.gz
+sudo tar xzf pycharm-community-$PYCHARM_VERSION.tar.gz -C /opt/
+rm pycharm-community-$PYCHARM_VERSION.tar.gz
+sudo mv /opt/pycharm-community-$PYCHARM_VERSION /opt/pycharm
 sudo ln -s /opt/pycharm/bin/pycharm.sh /usr/bin/pycharm
-
