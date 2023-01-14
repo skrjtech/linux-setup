03aff95e9c21bb1a113b29c35fff968f3db45ba3
skrjtech <skrjtech@skrtech.com>
2023-01-15 07:10:33 +0900

    update

--- archives/dockerProjects/python/install.sh
+++ archives/dockerProjects/python/install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 IMAGEOS=debian
-PLATFORM=linux/$1
 PYTHON_VERSIONS=(
-    3.11.1
-    # 3.10.9
-    # 3.9.16
-    # 3.8.16
-    # 3.7.16
-    # 3.6.15
+    # 3.11.1
+    3.10.9
+    3.9.16
+    3.8.16
+    3.7.16
+    3.6.15
 )
 for ver in ${PYTHON_VERSIONS[@]}
 do  
     image_tag=skrjtech/python:$IMAGEOS-python$ver
     echo $image_tag
-    docker buildx build --platform $PLATFORM -t $image_tag --build-arg IMAGE_TAG=$IMAGEOS:latest --build-arg PYTHON_VERSION=$ver --push . 
+    docker buildx build --platform linux/amd64,linux/arm64 -t $image_tag --build-arg IMAGE_TAG=$IMAGEOS:latest --build-arg PYTHON_VERSION=$ver --push . 
     # docker push $image_tag
 done
