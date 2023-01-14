291cfbb73e695174212bc79b1d91876b562b6737
skrjtech <skrjtech@skrtech.com>
2023-01-15 03:33:27 +0900

    一部修正

--- archives/dockerProjects/python/install.sh
+++ archives/dockerProjects/python/install.sh
@@ -11,7 +11,7 @@ PYTHON_VERSIONS=(
 for ver in ${PYTHON_VERSIONS[@]}
 do  
     image_tag=skrjtech/python:$IMAGEOS-python$ver
+    echo $image_tag
     docker build -t $image_tag . --build-arg IMAGE_TAG=$IMAGEOS:latest --build-arg PYTHON_VERSION=$ver
     docker push $image_tag
-    # echo $image_tag
 done
